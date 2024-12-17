5d768f04124146b17632d754b003b4a90aaa5cf1
smatsainars <smats.ainars@gmail.com>
2024-12-17 13:25:35 +0200

    initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $fields['password'] = bcrypt($fields['password']);
+
+        $user = User::create($fields);
+
+        $token = $user->createToken('registration_token');
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email',$request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return [
+                'message' => 'incorect someting '
+            ];
+        }
+        
+        $token = $user->createToken('login_token');
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    public function logout(Request $request){
+        $request->user()->tokens('')->delete();
+
+        return [
+            'message' => 'Logged out '
+        ];
+    }
+}
